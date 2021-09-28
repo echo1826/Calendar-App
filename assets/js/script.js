38d54727a1ced794c113925e822def04f7d35bdb
Ethan C <ethan.cho1826@gmail.com>
2021-09-28 18:44:55 -0500

    added functionailty to grab text from calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 var headerEl = $("#currentDay");
 // console.log(headerEl);
 var dateEl = $("<p>");
+var timeBlock = $(".container");
 headerEl.append(dateEl);
 
 
-var today = function(){
-    var dateTime = moment().format("MMMM Do YYYY, hh:mm:ss a");
+var today = function () {
+    var dateTime = moment().format("MMMM Do YYYY hh:mm:ss a");
     dateEl.text(dateTime);
-    }
-    setInterval(today, 1000);
+}
+setInterval(today, 1000);
+
+
+
+function getInput() {
+    var inputGrab = $("#schedule").val();
+    console.log("Input was grabbed: " + inputGrab);
+}
+
+timeBlock.on("click", ".btn", getInput);
